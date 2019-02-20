c61fae007da2a42d6d476e5626f8a32492ff1feb
Pratik Abhyankar <abhyankarpratik@gmail.com>
2019-02-20 10:31:28 -0500

    Add include zip.h and install libzip-dev. Add vscode config files

--- zipfuse.c
+++ zipfuse.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <zip.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello world!");
+
+    if (argc < 3) {
+        printf("\nThis program requires 2 arguments to work: \n");
+        printf("    1. Zip archive\n");
+        printf("    2. Mount point directory\n");
+        printf("usage: ./zipfuse <file.zip> <mount_point>\n\n");
+        exit(1);
+    }
+
+    char* archive = argv[1];
+    printf("\n");
+    printf("%s", archive);
+    printf("\n");
+
     return 0;
 }
